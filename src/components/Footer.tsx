1a1fd96d142d2535d18a0f13bfa9f3b5eddb64bb
vlwkajrrhtlvekzz-arch <vlwkajrrhtlvekzz@gmail.com>
2025-11-26 10:08:43 +0900

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 py-6 mt-12">
-      <div className="container mx-auto text-center text-sm text-gray-600">
-        © {new Date().getFullYear()} GodofPOS. All rights reserved.
-      </div>
-    </footer>
-  );
-}
